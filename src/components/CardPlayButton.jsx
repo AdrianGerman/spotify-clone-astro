e2f0cf847480928f27c0dbbf3a5bab5f0fed7960
AdrianGerman <adrian.german1019@gmail.com>
2024-09-04 16:03:35 -0600

    transition arrangement and listening to a single playlist

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -2,19 +2,26 @@ import { Pause, Play } from "./Player"
 import { usePlayerStore } from "@/store/playerStore"
 
 export function CardPlayButton({ id }) {
-  const { currentSong, isPlaying, setIsPlaying, setCurrentSong } =
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state)
 
   const handleClick = () => {
+    setCurrentMusic({
+      playlist: {
+        id
+      }
+    })
     setIsPlaying(!isPlaying)
   }
+
+  const isPlayingPlayList = isPlaying && currentMusic?.playlist.id === id
 
   return (
     <button
       onClick={handleClick}
       className="card-play-button rounded-full bg-green-500 p-4"
     >
-      {isPlaying ? <Pause /> : <Play />}
+      {isPlayingPlayList ? <Pause /> : <Play />}
     </button>
   )
 }
