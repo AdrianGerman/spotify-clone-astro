a1bc65f17e9094060cef764b45999e8b38859744
AdrianGerman <adrian.german1019@gmail.com>
2024-09-06 07:58:40 -0600

    fixing bugs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -86,20 +86,25 @@ const SongControl = ({ audio }) => {
 
   return (
     <div className="flex gap-x-3 text-xs pt-2">
-      <span className="opacity-50">{formatTime(currentTime)}</span>
+      <span className="opacity-50 w-12 text-right">
+        {formatTime(currentTime)}
+      </span>
 
       <Slider
         defaultValue={[0]}
         value={[currentTime]}
         max={audio?.current?.duration ?? 0}
         min={0}
-        className="w-[500px]"
+        className="w-[400px]"
         onValueChange={(value) => {
           const [newCurrentTime] = value
           audio.current.currentTime = newCurrentTime
         }}
       />
-      <span className="opacity-50">{formatTime(duration)}</span>
+
+      <span className="opacity-50 w-12">
+        {duration ? formatTime(duration) : "0:00"}
+      </span>
     </div>
   )
 }
@@ -178,8 +183,8 @@ export function Player() {
   }
 
   return (
-    <div className="flex flex-row justify-between w-full px-4 z-50">
-      <div>
+    <div className="flex flex-row justify-between w-full px-1 z-50">
+      <div className="w-[250px]">
         <CurrentSong {...currentMusic.song} />
       </div>
       <div className="grid place-content-center gap-4 flex-1">
